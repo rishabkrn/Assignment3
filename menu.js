f00fc1dc743e2f75955d2788cd28c2de79ecf9ce
rishabkrn <101308348+rishabkrn@users.noreply.github.com>
2022-05-29 18:21:38 +1000

    footer dev

--- menu.js
+++ menu.js
@@ -1,9 +1,10 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "500px";
+    document.getElementById("sidenav").style.width = "500px";
 }
   
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("sidenav").style.width = "0";
 }
+
