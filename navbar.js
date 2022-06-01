9621499e568ddf592a5de0d4490d976c99f4b3a5
rishabkrn <101308348+rishabkrn@users.noreply.github.com>
2022-06-01 17:19:02 +1000

    update homepage

---
+++ navbar.js
@@ -0,0 +1,9 @@
+/* width of the side navigation */
+function openNav() {
+    document.getElementById("sidenav").style.width = "400px";
+}
+  
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+}
+
