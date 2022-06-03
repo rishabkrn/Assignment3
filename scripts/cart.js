8c67be85c10944293120023cac0d0c6a320de8fc
Rishab Kiran <rishabk@student.unimelb.edu.au>
2022-06-04 02:46:18 +1000

    responsive design

---
+++ scripts/cart.js
@@ -0,0 +1,11 @@
+function inc(element) {
+    let el = document.querySelector(`[name="${element}"]`);
+    el.value = parseInt(el.value) + 1;
+  }
+  
+  function dec(element) {
+    let el = document.querySelector(`[name="${element}"]`);
+      if (parseInt(el.value) > 0) {
+        el.value = parseInt(el.value) - 1;
+    }
+  }
